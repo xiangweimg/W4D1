e3ff070436d74f1e6ba9acc71576671e92b06761
Xiangwei Liu <xiangweimg@gmail.com>
2022-08-08 13:33:59 -0400

    children

--- W4D1/skeleton/lib/tic_tac_toe_node.rb
+++ W4D1/skeleton/lib/tic_tac_toe_node.rb
@@ -2,6 +2,9 @@ require_relative 'tic_tac_toe'
 
 class TicTacToeNode
   def initialize(board, next_mover_mark, prev_move_pos = nil)
+    @board = Board.new
+    @next_mover_mark = next_mover_mark
+    @prev_move_pos = prev_move_pos
   end
 
   def losing_node?(evaluator)
@@ -13,5 +16,25 @@ class TicTacToeNode
   # This method generates an array of all moves that can be made after
   # the current move.
   def children
+      children_arr = []
+      moves = self.find_valid_move
+      moves.each do |position| #position
+        children_arr << TicTacToeNode.new(board, next_mover_mark,position)
+      end
+    end
   end
+
+  def find_valid_move
+    valid_move = []
+    (0...@board.length).each do |idx1|
+      (0...@board.length).each do |idx2|
+        current = [idx1,idx2]
+        if @board.empty?(current)
+          valid_move << current
+        end
+      end
+    end
+    valid_move
+  end
+
 end
